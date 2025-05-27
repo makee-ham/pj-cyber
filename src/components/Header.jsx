ce5ca24419f1f30652c1c8a89ddac79eb0ea8666
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-05-27 15:11:01 +0900

    feat: Header 컴포넌트 생성 (Mobile & PC) (#11)
    * 💄 #4 Header(Mobile), Header Top(Desktop) 제작
    
    components 폴더의 Header.jsx에 구축, 필요한 svg 파일들 assets/images에 추가
    
    * 🎨 #4 주석으로 요소 구분
    
    주석을 각각 달아 헤더를 구성하는 요소를 구분했다
    
    * ✨ #4 Link(a) 필요한 부분에 태그 추가
    
    로고, 아이콘 목록에 Link 태그 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,72 @@
+import logoBlack from '../assets/images/logo_black.svg';
+import burger from '../assets/images/Burger.svg';
+import search from '../assets/images/Search.svg';
+import favorites from '../assets/images/Favorites.svg';
+import cart from '../assets/images/Cart.svg';
+import user from '../assets/images/User.svg';
+import { useLocation, Link } from 'react-router-dom';
+
 export default function Header() {
-  return <div className="text-3xl font-bold underline">Header</div>;
+  const location = useLocation();
+
+  const navItems = [
+    { path: '/', label: 'Home' },
+    { path: '/about', label: 'About' },
+    { path: '/contact-us', label: 'Contact Us' },
+    { path: '/blog', label: 'Blog' },
+  ];
+
+  return (
+    <header className="w-full max-w-[1120px] bg-white px-4 md:px-0 py-6 md:py-4 flex justify-between md:justify-center md:gap-8 items-center">
+      {/* 로고 */}
+      <Link to="/">
+        <img src={logoBlack} alt="cyber logo" className="h-7 flex-none" />
+      </Link>
+
+      {/* 검색창 */}
+      <form className="hidden md:flex flex-[4] items-center rounded-lg bg-[#f5f5f5] h-14 pr-10">
+        <img src={search} alt="search icon" className="w-6 h-6 m-4 mr-2" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-transparent text-sm focus:outline-none text-[#656565] flex-[1]"
+        />
+      </form>
+
+      {/* 네비게이션 */}
+      <nav className="hidden md:flex flex-[3] min-w-[210px] justify-between">
+        {navItems.map(({ path, label }) => (
+          <Link
+            key={path}
+            to={path}
+            className={
+              location.pathname === path
+                ? 'opacity-100'
+                : 'opacity-30 hover:opacity-100'
+            }
+          >
+            {label}
+          </Link>
+        ))}
+      </nav>
+
+      {/* 아이콘 목록 */}
+      <div className="hidden md:flex flex-[1] min-w-[96px] justify-between">
+        <Link>
+          <img src={favorites} alt="favorites icon" className="w-8 h-8" />
+        </Link>
+        <Link>
+          <img src={cart} alt="cart icon" className="w-8 h-8" />
+        </Link>
+        <Link>
+          <img src={user} alt="user icon" className="w-8 h-8" />
+        </Link>
+      </div>
+
+      {/* 햄버거 메뉴 */}
+      <button className="md:hidden">
+        <img src={burger} alt="burger button" className="w-10 h-10" />
+      </button>
+    </header>
+  );
 }
