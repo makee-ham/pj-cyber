761f39dde0e47fac2a39b0a265368f1855a7d17a
Makee <ina0312@naver.com>
2025-05-29 06:04:58 +0900

    📝 #14 버튼 분기 정리
    공통 스타일, 고스트 버튼 특징 스타일, filled 버튼 특징 스타일 구분하여 정리

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,7 @@
-export default function Button() {
+// 공통: 세로 크기 고정, 가로 크기 유동적, 폰트 수평+수직 중앙 정렬
+// 고스트: 높이 56px, 폰트 16px Medium, 버튼 색과 폰트컬러 일치, border-radius 6px
+// filled: 높이 48px, 폰트 14px medium, 버튼 색과 폰트컬러 반대, border-radius 8px
+
+export default function Button({ variant = 'filled', children }) {
   return <div>Button</div>;
 }
