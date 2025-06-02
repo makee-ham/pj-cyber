6046ab508b6ca4aadafa3895cd3b616efeaeeab4
Makee <ina0312@naver.com>
2025-06-02 15:42:09 +0900

    ✨ #16 FeaturedProduct 카드 컴포넌트 모바일 버전 완료
    FeaturedProducts에 들어가는 FeaturedProduct 모바일 버전 작업

---
+++ src/components/FeaturedProduct.jsx
@@ -0,0 +1,63 @@
+import Button from './Button';
+
+export default function FeaturedProduct({
+  title,
+  description,
+  bgColor,
+  textColor = 'black',
+  mobileImg,
+  hasButton = false,
+  hightlightTitleIndex = -1,
+}) {
+  const titleArr = title.split(' ');
+
+  return (
+    // gap 24px
+    // tailwind는 빌드 타임에 사용된 클래스만 인식하기에, bgColor같은 런타임 변수를 그대로 사용하면 미리 파싱을 안 했기에 적용 안 됨 (명시적으로 class 이름 넘겨야 함)
+    // Button.jsx 때처럼 경우를 나누어 문자열로 class를 더해줘야 함. 아니면 tailwind-merge 쓰든가.
+    // 이에 style로 분리
+    <article
+      className="flex flex-col items-center gap-8 w-full px-4 py-10"
+      style={{ backgroundColor: bgColor }}
+    >
+      <img
+        src={mobileImg}
+        alt="featured"
+        className="object-contain max-h-[200px]"
+      />
+      <div
+        className="flex flex-col items-center gap-4 w-full"
+        style={{ color: textColor }}
+      >
+        <h2 className="text-[34px] leading-10 text-center">
+          {titleArr.map((word, i) =>
+            i === hightlightTitleIndex ||
+            (hightlightTitleIndex === -1 && i === titleArr.length - 1) ? (
+              <strong key={i} className="font-semibold">
+                {word + ' '}
+              </strong>
+            ) : (
+              <span key={i} className="font-light">
+                {word + ' '}
+              </span>
+            ),
+          )}
+        </h2>
+        <p className="text-[#909090] leading-6 text-center text-base font-medium">
+          {description}
+        </p>
+        {hasButton ? (
+          <Button
+            variant="ghost"
+            color="black"
+            disabled={false}
+            children
+            className="w-full"
+          >
+            Shop Now
+          </Button>
+        ) : null}
+      </div>
+    </article>
+  );
+}
--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,1 +1,39 @@
-export default function FeaturedProducts() {}
+import FeaturedProduct from './FeaturedProduct';
+import airpodsM from '../assets/images/airpodsmax_m.png';
+import visionM from '../assets/images/applevision_m.png';
+import psM from '../assets/images/playstation_m.png';
+import macbookM from '../assets/images/macbookpro14_m.png';
+
+export default function FeaturedProducts() {
+  return (
+    <section>
+      <FeaturedProduct
+        title="Apple AirPods Max"
+        description="Computational audio. Listen, it's powerful"
+        bgColor="#EDEDED"
+        mobileImg={airpodsM}
+      />
+      <FeaturedProduct
+        title="Apple Vision Pro"
+        description="An immersive way to experience entertainment"
+        bgColor="#353535"
+        textColor="white"
+        mobileImg={visionM}
+      />
+      <FeaturedProduct
+        title="Playstation 5"
+        description="Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience."
+        bgColor="#fff"
+        mobileImg={psM}
+      />
+      <FeaturedProduct
+        title="Macbook Air"
+        description="The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display."
+        bgColor="#EDEDED"
+        mobileImg={macbookM}
+        hasButton={true}
+        hightlightTitleIndex={0}
+      />
+    </section>
+  );
+}
