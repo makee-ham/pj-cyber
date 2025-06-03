ebc235afdfe21e0fc70193783f0f1368343eb688
Makee <ina0312@naver.com>
2025-06-03 20:54:36 +0900

    📝 #17 BrowseByCategory 페이지 버튼 핸들 함수 추가, homeCategories 데이터 연결

--- src/components/BrowseByCategory.jsx
+++ src/components/BrowseByCategory.jsx
@@ -1,7 +1,20 @@
 import ArrowLeft from '~icons/ArrowLeft.svg';
 import ArrowRight from '~icons/ArrowRight.svg';
+import { categoryData, CATEGORIES_PER_PAGE } from '~data/homeCategories.js';
+import { useState } from 'react';
 
 export default function BrowseByCategory() {
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const maxPage = Math.ceil(categoryData.length / CATEGORIES_PER_PAGE) - 1;
+
+  const handleGoPrev = () =>
+    setPageIndex(prev => {
+      prev <= 0 ? maxPage : prev - 1;
+    });
+  const handleGoNext = () =>
+    setPageIndex(prev => (prev >= maxPage ? 0 : prev + 1));
+
   return (
     <section className="flex justify-center items-center w-full py-16 px-4">
       <div
@@ -16,10 +29,10 @@ export default function BrowseByCategory() {
             Browse By Category
           </h3>
           <div className="flex justify-between w-20 h-8">
-            <button>
+            <button onClick={handleGoPrev}>
               <img src={ArrowLeft} alt="arrow" />
             </button>
-            <button>
+            <button onClick={handleGoNext}>
               <img src={ArrowRight} alt="arrow" />
             </button>
           </div>
