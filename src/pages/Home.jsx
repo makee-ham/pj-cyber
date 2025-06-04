ff23df98414aa22bff3e7962bfcc3314f29a9d16
Makee <ina0312@naver.com>
2025-06-04 17:43:36 +0900

    chore: Home.jsx에 ProductCard 연결 import문 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import BrowseByCategory from '~components/BrowseByCategory';
 import FeaturedProducts from '~components/FeaturedProducts';
 import HeroSection from '~components/HeroSection';
-import ProductCard from '~components/ProductCard';
 
 export default function Home() {
   return (
