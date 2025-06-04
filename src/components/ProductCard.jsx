4aaef7f289e005c484386ad2efebebcaacb56ac5
Makee <ina0312@naver.com>
2025-06-04 17:20:00 +0900

    ✨ #26 ProductCard 모바일 버전 UI 작업 완료
    isLiked 상태 관리 통한 Like svg 색상변경, 전체적인 구조 flex로 잡기

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,52 @@
 import Like from '~icons/Like.svg';
 import TempImg from '~images/product_temp_mm.jpeg';
+import Button from './Button';
+import { useState } from 'react';
 
 export default function ProductCard() {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const TEMP_TITLE =
+    'Image Of A Smiling My Melody Holding A Kitchen Knife | BE CAREFUL: She Has No Mercy';
+  const TEMP_PRICE = 1998;
+
   return (
-    <div className="flex flex-col w-auto h-[352px] px-3 py-6 bg-[#F6F6F6] rounded-[9px]">
+    <div className="flex flex-col items-center gap-2 w-auto h-[352px] px-3 py-6 bg-[#F6F6F6] rounded-[9px]">
       <div className="flex justify-end w-full h-auto">
-        <img src={Like} alt="like" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          className="cursor-pointer"
+          onClick={() => setIsLiked(!isLiked)}
+        >
+          <path
+            d="M5.93415 18.5443L15.3152 27.3568C15.6397 27.6616 15.8019 27.814 15.9999 27.814C16.1979 27.814 16.3602 27.6616 16.6846 27.3568L26.0657 18.5443C28.6739 16.0941 28.9907 12.0621 26.797 9.23477L26.3845 8.70313C23.7603 5.32078 18.4928 5.88803 16.6488 9.75154C16.3883 10.2973 15.6115 10.2973 15.351 9.75154C13.5071 5.88803 8.23955 5.32078 5.61531 8.70313L5.20284 9.23477C3.00918 12.0621 3.32592 16.0941 5.93415 18.5443Z"
+            fill={`${isLiked ? '#f00' : 'transparent'}`}
+            stroke={`${isLiked ? '#f00' : '#909090'}`}
+            stroke-opacity="0.77"
+            stroke-width="1.4"
+            className="transition-[stroke,fill] duration-300 ease-in-out"
+          />
+        </svg>
       </div>
       <img
         src={TempImg}
         alt="product image"
         className="w-[104px] h-[104px] object-contain"
       />
+      <div className="flex flex-col w-full items-center gap-4">
+        <div className="flex flex-col items-center gap-4 w-full">
+          <h4 className="text-center text-base font-medium leading-6 line-clamp-2">
+            {TEMP_TITLE}
+          </h4>
+          <p className="text-center text-2xl font-semibold leading-6 tracking-[3%]">
+            ${TEMP_PRICE}
+          </p>
+        </div>
+        <Button className="w-full">Buy Now</Button>
+      </div>
     </div>
   );
 }
