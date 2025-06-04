0a8f77a4d2844e55b8209a0765484a26343d8f2e
Makee <ina0312@naver.com>
2025-06-04 16:16:21 +0900

    💄 #26 Like.svg 추가
    ProductCard에 들어가는 아이콘 다운로드

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 export default function ProductCard() {
   return (
     <div className="flex flex-col w-auto h-[352px] px-3 py-6 bg-[#F6F6F6] rounded-[9px]">
-      ProductCard height fixed
+      <div></div>
     </div>
   );
 }
