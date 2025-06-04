5a27b3465805299e12efd93930ac13b4da3af3c6
Makee <ina0312@naver.com>
2025-06-04 16:14:16 +0900

    🎨 #26 ProductCard.jsx 기본 구조 잡기
    export, return div 작업

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,7 @@
 export default function ProductCard() {
-  return <div>ProductCard</div>;
+  return (
+    <div className="flex flex-col w-auto h-[352px] px-3 py-6 bg-[#F6F6F6] rounded-[9px]">
+      ProductCard height fixed
+    </div>
+  );
 }
