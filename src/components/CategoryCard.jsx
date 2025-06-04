c9affbdf6e255761e18259931522535574742cd3
Makee <ina0312@naver.com>
2025-06-04 22:28:54 +0900

    💄 Style: CategoryCard에 transition & duration 추가
    통일감을 위하여

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,6 +1,6 @@
 export default function CategoryCard({ imgSrc, name }) {
   return (
-    <div className="flex-1 flex flex-col justify-center items-center gap-2 min-w-36 h-32 bg-[#EDEDED] hover:bg-[#EDEDED]/60 rounded-[15px] cursor-pointer">
+    <div className="flex-1 flex flex-col justify-center items-center gap-2 min-w-36 h-32 bg-[#EDEDED] hover:bg-[#EDEDED]/60 rounded-[15px] cursor-pointer transition-colors duration-300">
       <img src={imgSrc} alt="categories" className="w-12 object-contain" />
       <p className="leading-6 text-balance">{name}</p>
     </div>
