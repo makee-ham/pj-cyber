1c8b73166c53576fc4f4806fc539c9ac94efba8c
Makee <ina0312@naver.com>
2025-06-04 17:40:27 +0900

    🔥 #26 Home.jsx의 테스트용 ProductCard 컴포넌트 태그 삭제
    테스트 일단 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import ProductCard from '~components/ProductCard';
 export default function Home() {
   return (
     <div>
-      <ProductCard />
       <HeroSection />
       <FeaturedProducts />
       <BrowseByCategory />
