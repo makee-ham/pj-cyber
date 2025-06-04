c90e667ca6f329e99b274b351b2f600c95267f39
Makee <ina0312@naver.com>
2025-06-04 16:27:19 +0900

    📝 #26 원활한 테스트 위해 Home.jsx 페이지에 ProductCard 넣어둠
    화면에서 안 보이면 작업하기 힘듦

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,17 @@
+import Like from '~icons/Like.svg';
+import TempImg from '~images/product_temp_mm.jpeg';
+
 export default function ProductCard() {
   return (
     <div className="flex flex-col w-auto h-[352px] px-3 py-6 bg-[#F6F6F6] rounded-[9px]">
-      <div></div>
+      <div className="flex justify-end w-full h-auto">
+        <img src={Like} alt="like" />
+      </div>
+      <img
+        src={TempImg}
+        alt="product image"
+        className="w-[104px] h-[104px] object-contain"
+      />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import BrowseByCategory from '~components/BrowseByCategory';
 import FeaturedProducts from '~components/FeaturedProducts';
 import HeroSection from '~components/HeroSection';
+import ProductCard from '~components/ProductCard';
 
 export default function Home() {
   return (
     <div>
+      <ProductCard />
       <HeroSection />
       <FeaturedProducts />
       <BrowseByCategory />
