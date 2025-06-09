481d1b9d812698067ece9c9b93fa0ee03ab96a26
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-06-09 21:34:01 +0900

    feat: 메타 데이터를 동적으로 설정하는 Meta 컴포넌트 만들기 (#35) (#36)
    * ➕ #34 react-helmet 설치
    
    meta tag 작성 목적
    
    * 📝 #34 make Meta.jsx in components
    
    * ✨ #34 Meta 컴포넌트 태그 설정
    
    다나와, 컴퓨존 참고 + MDN 문서와 Open Graph 참고
    
    * ✨ #34 Home.jsx에 Meta 컴포넌트 추가
    
    최상단에 추가
    
    * 🎨 #34 Meta.jsx 디폴트 값 세팅
    
    title, description, keywords, image, url에 디폴트 값 세팅

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,18 @@ import BrowseByCategory from '~components/BrowseByCategory';
 import FeaturedProducts from '~components/FeaturedProducts';
 import HeroSection from '~components/HeroSection';
 import MainProducts from '~components/MainProducts';
+import Meta from '~components/Meta';
 
 export default function Home() {
   return (
-    <div>
-      <HeroSection />
-      <FeaturedProducts />
-      <BrowseByCategory />
-      <MainProducts />
-    </div>
+    <>
+      <Meta />
+      <div>
+        <HeroSection />
+        <FeaturedProducts />
+        <BrowseByCategory />
+        <MainProducts />
+      </div>
+    </>
   );
 }
