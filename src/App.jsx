e819c0ec2957e15d3a8acd8f27f512ac2b1f989e
Makee <ina0312@naver.com>
2025-06-09 20:38:24 +0900

    📝 #34 make Meta.jsx in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import AppRouter from './router';
+import { Helmet } from 'react-helmet';
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <Helmet></Helmet>
+      <AppRouter />
+    </>
+  );
 }
