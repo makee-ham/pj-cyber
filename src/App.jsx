643bfc3ff837ac4ed6604526e3266c48eac7ec61
Makee <ina0312@naver.com>
2025-06-09 21:09:56 +0900

    ✨ #34 Home.jsx에 Meta 컴포넌트 추가
    최상단에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import AppRouter from './router';
-import { Helmet } from 'react-helmet';
 
 export default function App() {
-  return (
-    <>
-      <Helmet></Helmet>
-      <AppRouter />
-    </>
-  );
+  return <AppRouter />;
 }
