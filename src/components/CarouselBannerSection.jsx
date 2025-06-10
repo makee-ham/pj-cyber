98e351d43a25de745fbead7ad889060451e5cda0
Makee <ina0312@naver.com>
2025-06-10 17:59:54 +0900

    🎨 #30 CarouselBannerSection 큰 구조 잡기
    이후 세부 구조 잡을 예정

---
+++ src/components/CarouselBannerSection.jsx
@@ -0,0 +1,8 @@
+export default function CarouselBannerSection() {
+  return (
+    <section className="w-full py-14 px-8 overflow-hidden md:hidden">
+      <div className=""></div>
+      <ul></ul>
+    </section>
+  );
+}
