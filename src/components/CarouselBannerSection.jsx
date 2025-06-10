335859f57de8108c8fbda22dfc8a2e396d200139
Makee <ina0312@naver.com>
2025-06-10 18:08:54 +0900

    ✨ #30 dot indicator 컴포넌트 CarouselDots 파일 생성

--- src/components/CarouselBannerSection.jsx
+++ src/components/CarouselBannerSection.jsx
@@ -1,6 +1,6 @@
 export default function CarouselBannerSection() {
   return (
-    <section className="w-full py-14 px-8 overflow-hidden md:hidden">
+    <section className="w-full py-14 px-8 flex flex-col gap-12 overflow-hidden md:hidden">
       <div className=""></div>
       <ul></ul>
     </section>
